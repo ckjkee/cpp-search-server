052b144332c78a7249b26291909d917018856ec3
ckjkee <gonnabejj@gmail.com>
2023-07-26 14:22:15 +0300

    "My first commit"

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -2,3 +2,26 @@
 // Напишите ответ здесь:
 
 // Закомитьте изменения и отправьте их в свой репозиторий.
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   
+    int result = 0;
+    for( int i = 1; i <= 1000; ++i){
+        if(i % 10 == 3){
+            ++result;
+        }
+        else if(((i / 10))% 10 == 3){
+            ++result;
+        }
+        else if((i / 100) == 3){
+            ++result;
+        }
+    }
+    cout << result << endl;
+   
+    return 0;
+}
